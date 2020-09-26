23dc156b505695d784aef21df1df70d5127a41a3
Alberto Pou <albertopouquiros@gmail.com>
2020-09-26 17:00:50 +0200

    Move aliases to right file

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -49,3 +49,23 @@ alias bcconfs="cd $HOME/roi/configurations/"
 # Skitude
 alias skitude="cd $HOME/skitude/"
 alias cm="cd $HOME/skitude/customer-management/"
+
+# Enable handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# Some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
