aff2a0dcc55547ec3e7be82682eee9661787cf6e
Alberto Pou <albertopouquiros@gmail.com>
2020-10-31 18:21:23 +0100

    Fixes Ubuntu 20

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -7,7 +7,7 @@ ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 
 mkdir -p ~/.config
 mkdir -p ~/.config/i3
-ln -sf ~/.dotfiles/linux/i3/.settings .config/i3/config
+ln -sf ~/.dotfiles/linux/i3/.settings ~/.config/i3/config
 
 mkdir -p ~/.config/terminator
 ln -sf ~/.dotfiles/linux/terminator/.settings ~/.config/terminator/config
